7288378e030d74bb30db19b9dab75e476de21669
Austin <austin@Ennovar-MarCom13.local>
2016-08-10 15:31:00 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  blogs
+} from './blogs';
+// a lot like this
+// var BlogPost = {
+//   render: function() {
+//     return (
+//       <div>
+//       </div>
+//     )
+//   }
+// }
+class BlogPost extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <h3>{ this.props.blog.title }</h3>
+        <hr/>
+        <p>
+          { this.props.blog.body }
+        </p>
+      </div>
+    );
+  }
+}
+class BlogFeed extends Component {
+  getBlogPosts() {
+    let blogs = [];
+    for (let i = 0; i < this.props.blogs.length; i++) {
+      let blog = this.props.blogs[i]
+      blogs.push(<BlogPost blog={blog} />);
+    }
+    return blogs;
+  }
+  render() {
+    let blogs = this.getBlogPosts();
+    return (
+      <div>
+        { blogs }
+      </div>
+    )
+  }
+}
+class WeatherWidget extends Component {
+  render() {
+    return(
+      <h3>It is HOTTT!!!!</h3>
+    )
+  }
+}
+// <div class="App">
+class App extends Component {
+  getBlogPosts() {
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Austins Blog</h2>
+        </div>
+        <p className="App-blogfeed">
+          <WeatherWidget />
+          <BlogFeed blogs={ blogs } />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
